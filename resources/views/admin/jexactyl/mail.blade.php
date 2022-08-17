125dbef5c574c37dd6acc42298a50cd29ffb702a
Koru Studios <44905402+justkoru-txc@users.noreply.github.com>
2022-08-17 10:54:10 +0200

    Update mail.blade.php

--- resources/views/admin/jexactyl/mail.blade.php
+++ resources/views/admin/jexactyl/mail.blade.php
@@ -2,11 +2,11 @@
 @include('partials/admin.jexactyl.nav', ['activeTab' => 'mail'])
 
 @section('title')
-    Jexactyl Mail
+    Dashy Mail
 @endsection
 
 @section('content-header')
-    <h1>Mail Settings<small>Configure how Pterodactyl should handle sending emails.</small></h1>
+    <h1>Mail Settings<small>Configure how Dashy should handle sending emails.</small></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">Admin</a></li>
         <li class="active">Settings</li>
